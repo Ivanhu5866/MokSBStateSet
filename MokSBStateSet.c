da99fe881c65d3b4a591aed1a7e7ad2293450177
Ivan Hu <ivan.hu@canonical.com>
2016-08-10 15:31:00 +0800

    init files
    Signed-off-by: Ivan Hu <ivan.hu@canonical.com>

---
+++ MokSBStateSet.c
@@ -0,0 +1,49 @@
+/*
+ *  Copyright(C) 2016 Canonical Ltd.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+ *  USA.
+ */
+
+#include <Uefi.h>
+#include <Library/UefiLib.h>
+#include <Library/UefiApplicationEntryPoint.h>
+
+#define  MOKSBSTATE_GUID    \
+{ 0x605DAB50, 0xE046, 0x4300, {0xab, 0xb6, 0x3d, 0xd8, 0x10, 0xdd, 0x8b, 0x23}}
+
+EFI_STATUS
+EFIAPI
+UefiMain (
+  IN EFI_HANDLE        ImageHandle,
+  IN EFI_SYSTEM_TABLE  *SystemTable
+  )
+{
+  UINT32        VariableAttr;
+  EFI_GUID      VariableMoksbGuid = MOKSBSTATE_GUID;
+  UINT8         Data=1;
+
+  VariableAttr = (EFI_VARIABLE_NON_VOLATILE|EFI_VARIABLE_BOOTSERVICE_ACCESS);	
+
+  SystemTable->RuntimeServices->SetVariable (
+         L"MokSBState",     
+         &VariableMoksbGuid,   
+         VariableAttr,        
+         1, 
+         &Data          
+         );
+
+  return EFI_SUCCESS;
+}
