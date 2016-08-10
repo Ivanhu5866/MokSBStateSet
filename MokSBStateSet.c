9cfae11c7af6598fbf28ae5d0ca48d47c7d26e71
Ivan Hu <ivan.hu@canonical.com>
2016-08-10 15:40:05 +0800

    add compile with gnuefi
    Signed-off-by: Ivan Hu <ivan.hu@canonical.com>

--- MokSBStateSet.c
+++ MokSBStateSet.c
@@ -17,33 +17,30 @@
  *  USA.
  */
 
-#include <Uefi.h>
-#include <Library/UefiLib.h>
-#include <Library/UefiApplicationEntryPoint.h>
+#include <efi.h>
+#include <efilib.h>
 
 #define  MOKSBSTATE_GUID    \
 { 0x605DAB50, 0xE046, 0x4300, {0xab, 0xb6, 0x3d, 0xd8, 0x10, 0xdd, 0x8b, 0x23}}
 
 EFI_STATUS
 EFIAPI
-UefiMain (
-  IN EFI_HANDLE        ImageHandle,
-  IN EFI_SYSTEM_TABLE  *SystemTable
-  )
+efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
 {
-  UINT32        VariableAttr;
-  EFI_GUID      VariableMoksbGuid = MOKSBSTATE_GUID;
-  UINT8         Data=1;
+	UINT32 VariableAttr;
+	EFI_GUID VariableMoksbGuid = MOKSBSTATE_GUID;
+	UINT8 Data=1;
 
-  VariableAttr = (EFI_VARIABLE_NON_VOLATILE|EFI_VARIABLE_BOOTSERVICE_ACCESS);	
+	VariableAttr = (EFI_VARIABLE_NON_VOLATILE|EFI_VARIABLE_BOOTSERVICE_ACCESS);	
 
-  SystemTable->RuntimeServices->SetVariable (
-         L"MokSBState",     
-         &VariableMoksbGuid,   
-         VariableAttr,        
-         1, 
-         &Data          
-         );
+	InitializeLib(ImageHandle, SystemTable);
 
-  return EFI_SUCCESS;
+	uefi_call_wrapper(RT->SetVariable, 5,
+		L"MokSBState",
+		&VariableMoksbGuid,
+		VariableAttr,   
+		1,
+		&Data);
+
+	return EFI_SUCCESS;
 }
